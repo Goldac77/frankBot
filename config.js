ca485f2d453e6e033d974c10cf95d54ef4a5ccc8
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-05-07 22:44:44 +0000

    added a configuration file

---
+++ config.js
@@ -0,0 +1,18 @@
+const fs = require("node:fs");
+
+// Content of the .env file
+const envContent = `
+TOKEN="YOUR_BOT_TOKEN"
+CLIENTID="YOUR_BOT_APPLICATION_ID"
+GUILDID="YOUR_SERVER_ID"
+`;
+
+// Write the content to the .env file
+fs.writeFile('.env', envContent, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log('.env file created successfully.');
+});
